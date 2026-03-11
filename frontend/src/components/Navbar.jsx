00db81a1f4ad10622038e0b209d3325f257094bf
Sebastian906 <elsebas1912@gmail.com>
2026-03-10 23:18:19 -0500

    Integración de APIs de logeo y registro en el frontend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import { NavLink, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Navbar = () => {
 
     const navigate = useNavigate();
+    const { token, setToken } = useContext(AppContext)
     const [showMenu, setShowMenu] = useState(false);
-    const [token, setToken] = useState(true);
+
+    const logout = () => {
+        setToken(false)
+        localStorage.removeItem('token')
+    }
 
     return (
         <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-slate-500'>
@@ -53,7 +59,7 @@ const Navbar = () => {
                                         My Appointments
                                     </p>
                                     <p
-                                        onClick={() => setToken(false)}
+                                        onClick={logout}
                                         className='hover:text-slate-950 cursor-pointer'
                                     >
                                         Logout
